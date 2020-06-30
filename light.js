9270bd47c7d218f41b5ca6c109a617c29ba40f39
abdellahtamessouct <abdellahtame@gmail.com>
2020-06-30 14:08:36 +0100

    Add files via upload

---
+++ light.js
@@ -0,0 +1,13 @@
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+document.body.classList.toggle('dark');
+btnSwitch.classList.toggle('active');
+
+// Guardamos el modo en localstorage.
+if(document.body.classList.contains('dark')){
+localStorage.setItem('dark-mode', 'true');
+} else {
+localStorage.setItem('dark-mode', 'false');
+}
+});
